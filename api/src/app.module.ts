568f697c781ad3584aafb8bbd9d69c97f5aaf422
robsu17 <robson.costa134@gmail.com>
2026-05-16 10:16:00 -0300

    feat: add authentication and document modules, integrate environment configuration

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { DocumentModule } from './modules/document/document.module';
+import { AuthenticationModule } from './modules/authentication/authentication.module';
+import { ConfigModule } from '@nestjs/config';
+import { envSchema } from './config/env';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    DocumentModule,
+    AuthenticationModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: (config) => envSchema.parse(config),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
