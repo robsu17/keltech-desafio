a4a007038409de37c4b7218f34bde16d5c432351
robsu17 <robson.costa134@gmail.com>
2026-05-16 10:19:28 -0300

    feat(api): enhance bootstrap function to use ConfigService for dynamic port configuration

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,14 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import type { Env } from './config/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService<Env, true>);
+  const port = configService.get('PORT', { infer: true });
+  await app.listen(port);
+  Logger.log(`🚀 API running on http://localhost:${port}`, 'Bootstrap');
 }
 bootstrap();
